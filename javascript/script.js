6b6a713224fef4ee3c5a342c7475341680c03231
Kyle Soliveres <kyle.mngbt@gmail.com>
2024-09-06 20:41:12 +0800

    build: create a reference to the generate btn

--- javascript/script.js
+++ javascript/script.js
@@ -1,7 +1,9 @@
 const imgBox = document.querySelector("#img-box");
 const qrImage = document.querySelector("#qr-image");
 const qrText = document.querySelector("#qr-text");
+const generateBtn = document.querySelector(".container button");
 
 function generateQr() {
   qrImage.src = "https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=" + qrText.value;
 }
+
