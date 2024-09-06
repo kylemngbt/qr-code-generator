a499754a9c47f14cf2c99021fdf2a4512e155101
Kyle Soliveres <kyle.mngbt@gmail.com>
2024-09-06 20:53:41 +0800

    feat: modify generateQr func
    Will add the error class animation and remove it
    after 1000 miliseconds.

--- javascript/script.js
+++ javascript/script.js
@@ -7,7 +7,12 @@ function generateQr() {
   if (qrText.value.length > 0) {
     qrImage.src = "https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=" + qrText.value;
     imgBox.classList.add("show-img");
-  } 
+  } else {
+    qrText.classList.add("error");
+    setTimeout(() => {
+      qrText.classList.remove("error");
+    }, 1000)
+  }
 }
 
 generateBtn.addEventListener("click", generateQr);
