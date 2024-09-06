3111eeea9a905377ee280e3661c74fb51cc8a0d2
Kyle Soliveres <kyle.mngbt@gmail.com>
2024-09-06 20:40:22 +0800

    feat: create the initial generateQr func

--- javascript/script.js
+++ javascript/script.js
@@ -1,3 +1,7 @@
 const imgBox = document.querySelector("#img-box");
 const qrImage = document.querySelector("#qr-image");
 const qrText = document.querySelector("#qr-text");
+
+function generateQr() {
+  qrImage.src = "https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=" + qrText.value;
+}
