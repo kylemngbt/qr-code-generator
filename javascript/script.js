d0a8b174848ed1cc801714dad0026408b4f6f6e7
Kyle Soliveres <kyle.mngbt@gmail.com>
2024-09-06 20:47:16 +0800

    feat: modify the generateQr func

--- javascript/script.js
+++ javascript/script.js
@@ -4,8 +4,10 @@ const qrText = document.querySelector("#qr-text");
 const generateBtn = document.querySelector(".container button");
 
 function generateQr() {
-  qrImage.src = "https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=" + qrText.value;
-  imgBox.classList.add("show-img");
+  if (qrText.value.length > 0) {
+    qrImage.src = "https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=" + qrText.value;
+    imgBox.classList.add("show-img");
+  } 
 }
 
 generateBtn.addEventListener("click", generateQr);
